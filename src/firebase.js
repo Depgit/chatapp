fb3ad2279f81c1e52235b776433aa0d8e44cf1a9
Deepak Mishra <70760888+Depgit@users.noreply.github.com>
2021-02-27 17:22:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBjeLNlo5Ii3pqHJlfPLWVezre7yzO0YHM",
-  authDomain: "whatsapp-mern-c86a9.firebaseapp.com",
-  databaseURL: "https://whatsapp-mern-c86a9.firebaseio.com",
-  projectId: "whatsapp-mern-c86a9",
-  storageBucket: "whatsapp-mern-c86a9.appspot.com",
-  messagingSenderId: "157508844731",
-  appId: "1:157508844731:web:689a0a299e1e1e6c71bd9e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
